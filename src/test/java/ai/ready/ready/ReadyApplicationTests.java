c4e9ae6f7cb1f32dc9f660a1527cd66d7b2a3a20
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2024-10-04 09:40:23 +0200

    Initial commit

---
+++ src/test/java/ai/ready/ready/ReadyApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.ready.ready;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
