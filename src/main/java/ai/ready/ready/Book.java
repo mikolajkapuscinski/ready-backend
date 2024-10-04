7058ab4f7456485cbd75c3d08d178a3c0a4cfa45
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2024-10-04 11:22:15 +0200

    feat: book entity

---
+++ src/main/java/ai/ready/ready/Book.java
@@ -0,0 +1,28 @@
+package ai.ready.ready;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Book {
+
+    protected Book() {}
+
+    public Book(String title, String author, int numberOfPages) {
+        this.title = title;
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String author;
+    private Integer numberOfPages;
+
+}
