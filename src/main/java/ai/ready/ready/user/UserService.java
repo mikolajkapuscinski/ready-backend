c0e2af6fa3f1edba324e8d07a3a637e4c2ce2797
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2024-10-31 12:46:34 +0100

    feat: UserService

---
+++ src/main/java/ai/ready/ready/user/UserService.java
@@ -0,0 +1,16 @@
+package ai.ready.ready.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public User login(final String email, final String password) {
+        return null;
+    }
+
+    public User register(final User user){
+        return null;
+    }
+
+}
