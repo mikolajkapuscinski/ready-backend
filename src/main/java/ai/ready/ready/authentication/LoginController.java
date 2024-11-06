61dadadc9e361a05ced762c5d5f2d1b24ab1e92c
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2024-11-06 20:35:15 +0100

    feat: spring SecurityConfig

---
+++ src/main/java/ai/ready/ready/authentication/LoginController.java
@@ -0,0 +1,11 @@
+package ai.ready.ready.authentication;
+
+import lombok.AllArgsConstructor;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class LoginController {
+
+}
