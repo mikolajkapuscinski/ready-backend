bebed8dcb90208461fe15af57b4c9b62ff259705
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2024-11-24 21:53:59 +0100

    feat: add JsonAuthFilter for authentication

---
+++ src/main/java/ai/ready/ready/security/authentication/JsonAuthFilter.java
@@ -0,0 +1,41 @@
+package ai.ready.ready.security.authentication;
+
+import ai.ready.ready.security.authentication.dto.LoginRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JsonAuthFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try{
+            BufferedReader reader = request.getReader();
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+            LoginRequest loginRequest = objectMapper.readValue(sb.toString(), LoginRequest.class);
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                    loginRequest.getEmail(),
+                    loginRequest.getPassword()
+            );
+            setDetails(request, token);
+            return this.getAuthenticationManager().authenticate(token);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
