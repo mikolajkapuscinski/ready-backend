eeb83e50807af3032842e064da8d882f07c6a995
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2024-11-24 21:54:30 +0100

    feat: add Success and Failure handlers for JsonAuthFilter

---
+++ src/main/java/ai/ready/ready/security/authentication/RestAuthSuccessHandler.java
@@ -0,0 +1,18 @@
+package ai.ready.ready.security.authentication;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class RestAuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        super.onAuthenticationSuccess(request, response, authentication);
+    }
+}
