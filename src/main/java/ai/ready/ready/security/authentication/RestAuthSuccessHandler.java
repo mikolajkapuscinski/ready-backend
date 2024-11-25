1379912affd4efa4121557cb57cdbd1985ef2c2d
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2024-11-25 09:02:12 +0100

    fix: establish httpsession

--- src/main/java/ai/ready/ready/security/authentication/RestAuthSuccessHandler.java
+++ src/main/java/ai/ready/ready/security/authentication/RestAuthSuccessHandler.java
@@ -4,15 +4,27 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
 public class RestAuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final SecurityContextRepository securityContextRepository  = new HttpSessionSecurityContextRepository();
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        setDefaultTargetUrl("/books");
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(authentication);
+        SecurityContextHolder.setContext(context);
+        securityContextRepository.saveContext(context, request, response);
         super.onAuthenticationSuccess(request, response, authentication);
     }
 }
