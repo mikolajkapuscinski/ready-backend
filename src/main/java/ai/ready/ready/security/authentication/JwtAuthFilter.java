8e808ca524435d65ebb2c7e6780816d2022a59a3
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2025-01-31 17:04:41 +0100

    feat: add jwt handling

---
+++ src/main/java/ai/ready/ready/security/authentication/JwtAuthFilter.java
@@ -0,0 +1,56 @@
+package ai.ready.ready.security.authentication;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import java.io.IOException;
+
+public class JwtAuthFilter extends BasicAuthenticationFilter {
+
+    private static final String TOKEN_HEADER = "Authorization";
+    private static final String TOKEN_PREFIX = "Bearer ";
+    private final UserDetailsService userDetailsService;
+    private final String secret;
+
+    public JwtAuthFilter(AuthenticationManager authenticationManager, UserDetailsService userDetailsService, String secret) {
+        super(authenticationManager);
+        this.userDetailsService = userDetailsService;
+        this.secret = secret;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        UsernamePasswordAuthenticationToken authentication = getAuthentication(request);
+        if (authentication != null) {
+            chain.doFilter(request, response);
+            return;
+        }
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(request, response);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+        String token = request.getHeader(TOKEN_HEADER);
+        if (token != null && token.startsWith(TOKEN_PREFIX)) {
+            String username = JWT.require(Algorithm.HMAC512(secret))
+                    .build()
+                    .verify(token.replace(TOKEN_PREFIX, ""))
+                    .getSubject();
+            if (username != null) {
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                return new UsernamePasswordAuthenticationToken(userDetails.getUsername(), null, userDetails.getAuthorities());
+            }
+        }
+        return null;
+    }
+}
--- src/main/java/ai/ready/ready/security/authentication/RestAuthSuccessHandler.java
+++ src/main/java/ai/ready/ready/security/authentication/RestAuthSuccessHandler.java
@@ -1,30 +1,46 @@
 package ai.ready.ready.security.authentication;
 
+import ai.ready.ready.security.authentication.dto.UserDetailsModel;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
-import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
-import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.io.IOException;
+import java.util.Date;
 
 @Component
 public class RestAuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-    private final SecurityContextRepository securityContextRepository  = new HttpSessionSecurityContextRepository();
+    private final long expirationTime;
+    private final String secret;
+
+    public RestAuthSuccessHandler(
+            @Value("${jwt.expirationTime}") long expirationTime,
+            @Value("${jwt.secret}") String secret
+    ) {
+        this.expirationTime = expirationTime;
+        this.secret = secret;
+    }
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        setDefaultTargetUrl("/books");
-        SecurityContext context = SecurityContextHolder.createEmptyContext();
-        context.setAuthentication(authentication);
-        SecurityContextHolder.setContext(context);
-        securityContextRepository.saveContext(context, request, response);
-        super.onAuthenticationSuccess(request, response, authentication);
+        UserDetailsModel userDetails = (UserDetailsModel) authentication.getPrincipal();
+        String token = JWT.create()
+                .withSubject(userDetails.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + expirationTime))
+                .sign(Algorithm.HMAC256(secret));
+        Cookie jwtCookie = new Cookie("token", token);
+        jwtCookie.setMaxAge((int) expirationTime);
+        jwtCookie.setPath("/");
+        jwtCookie.setHttpOnly(true);
+        jwtCookie.setSecure(true);
+        response.addCookie(jwtCookie);
     }
 }
