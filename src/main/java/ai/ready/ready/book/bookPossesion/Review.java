e5ba87e2206684663a6ca6c67c0ee36fcafff4df
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2025-02-08 21:30:21 +0100

    feat: create Review entity

---
+++ src/main/java/ai/ready/ready/book/bookPossesion/Review.java
@@ -0,0 +1,21 @@
+package ai.ready.ready.book.bookPossesion;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Review {
+    @Id
+    @Column(name = "review_id")
+    private Long id;
+    @OneToOne(fetch = FetchType.EAGER)
+    private BookPossession book;
+    private String title;
+    private Rating rating;
+    private String content;
+    private Integer likes;
+    private LocalDateTime createdAt;
+}
