27aa54bb2176dea81ac47cad595fc37743080985
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2025-02-09 20:37:07 +0100

    feat: create Rating Enum

---
+++ src/main/java/ai/ready/ready/book/bookPossesion/Rating.java
@@ -0,0 +1,25 @@
+package ai.ready.ready.book.bookPossesion;
+
+import lombok.Getter;
+
+@Getter
+public enum Rating {
+    ZERO(0d),
+    ZERO_AND_A_HALF(0.5),
+    ONE(1d),
+    ONE_AND_A_HALF(1.5),
+    TWO(2d),
+    TWO_AND_A_HALF(2.5),
+    THREE(3d),
+    THREE_AND_A_HALF(3.5),
+    FOUR(4d),
+    FOUR_AND_A_HALF(4.5),
+    FIVE(5d);
+
+    private final Double value;
+
+    Rating(Double value) {
+        this.value = value;
+    }
+
+}
