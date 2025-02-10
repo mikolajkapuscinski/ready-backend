443545af3313ddd6ae652833cc427cfc18f352cb
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2025-02-10 21:46:30 +0100

    feat: create Level record for calculate user level from exp points

---
+++ src/main/java/ai/ready/ready/user/expPoints/Level.java
@@ -0,0 +1,28 @@
+package ai.ready.ready.user.expPoints;
+
+
+import ai.ready.ready.user.User;
+
+public record Level(int level, double progression) {
+
+    static final int BASE_PROMOTION_THRESHOLD = 100;
+    static final double PROMOTION_THRESHOLD_MULTIPLIER = 1.2;
+
+    public static Level getUserLevel(User user) {
+        Long expPoints = user.getExpPoints();
+        if (expPoints == null) return new Level(0, 0);
+        int level = 1;
+        double progression;
+        double previousPromotionThreshold = 0;
+        double currentPromotionThreshold = BASE_PROMOTION_THRESHOLD;
+        while(currentPromotionThreshold <= expPoints) {
+            level++;
+            previousPromotionThreshold = currentPromotionThreshold;
+            currentPromotionThreshold += currentPromotionThreshold * PROMOTION_THRESHOLD_MULTIPLIER;
+        }
+
+        progression = (expPoints - previousPromotionThreshold)/(currentPromotionThreshold - previousPromotionThreshold);
+
+        return new Level(level, progression);
+    }
+}
