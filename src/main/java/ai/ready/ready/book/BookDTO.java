3aac406941da8217828859681506e20c627dceab
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2025-02-19 10:43:53 +0100

    feat: create BookDTO record

---
+++ src/main/java/ai/ready/ready/book/BookDTO.java
@@ -0,0 +1,25 @@
+package ai.ready.ready.book;
+
+import ai.ready.ready.bookPossesion.ReviewDTO;
+
+import java.util.Date;
+import java.util.List;
+
+public record BookDTO(
+        Long id,
+        String title,
+        String author,
+        String isbn13,
+        String isbn10,
+        String coverURL,
+        String language,
+        Integer numberOfPages,
+        String description,
+        Date dateOfPublication,
+        List<ReviewDTO> reviews,
+        Integer avgRating,
+        Integer numberOfToReads,
+        Integer numberOfCurrentlyReading,
+        Integer numberOfRead
+) {
+}
