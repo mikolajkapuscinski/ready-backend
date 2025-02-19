6bceb99fd83c53449e88898511111ed10da09437
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2025-02-19 10:44:06 +0100

    feat: create ReviewDTO record

---
+++ src/main/java/ai/ready/ready/bookPossesion/ReviewDTO.java
@@ -0,0 +1,15 @@
+package ai.ready.ready.bookPossesion;
+
+import ai.ready.ready.user.dto.UserDTO;
+
+import java.util.Date;
+
+public record ReviewDTO(
+        Long id,
+        String title,
+        UserDTO user,
+        String content,
+        Integer rating,
+        Date createdAt
+) {
+}
