b360d7b07fde8d8f75fbe516ede873e1858b5775
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2025-02-21 11:06:48 +0100

    feat: create review service

---
+++ src/main/java/ai/ready/ready/review/ReviewService.java
@@ -0,0 +1,34 @@
+package ai.ready.ready.review;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Limit;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+
+
+    public List<Review> getUserReviews(Long userId, int limit) {
+        return reviewRepository.findReviewsByUserId(userId, Limit.of(limit));
+    }
+
+    public List<Review> getBookReviews(Long bookId, int limit) {
+        return reviewRepository.findReviewsByBookId(bookId, Limit.of(limit));
+    }
+
+    public Integer calculateBookAvgRating(List<ReviewDTO> reviews) {
+        if (reviews == null || reviews.isEmpty()) {
+            return 0;
+        }
+        int sum = 0;
+        for (ReviewDTO review : reviews) {
+            sum += review.rating();
+        }
+        return sum / reviews.size();
+    }
+}
