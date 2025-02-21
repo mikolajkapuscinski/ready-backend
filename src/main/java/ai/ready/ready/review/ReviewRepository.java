b803ec5268a1f7986a956dac19602d82a7cc3d56
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2025-02-21 11:06:39 +0100

    feat: create review repository

---
+++ src/main/java/ai/ready/ready/review/ReviewRepository.java
@@ -0,0 +1,17 @@
+package ai.ready.ready.review;
+
+import org.springframework.data.domain.Limit;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ReviewRepository extends CrudRepository<Review, Long> {
+
+    @Query("Select r from Review r join BookPossession bp on bp.review.id = r.id where bp.user.id = :userId")
+    List<Review> findReviewsByUserId(@Param("userId") Long userId, Limit limit);
+
+    @Query("Select r from Review r join BookPossession bp on bp.review.id = r.id where bp.book.id = :bookId")
+    List<Review> findReviewsByBookId(@Param("bookId") Long bookId, Limit limit);
+}
