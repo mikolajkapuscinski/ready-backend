9d7653dbd08d509a9b17e455f80693eb44f911f8
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2025-02-27 21:20:38 +0100

    feat: create Format for chat response

---
+++ src/main/java/ai/ready/ready/ai/FindBookResponseFormat.java
@@ -0,0 +1,14 @@
+package ai.ready.ready.ai;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+public record FindBookResponseFormat(
+        @JsonProperty(required = true, value = "title") String title,
+        @JsonProperty(required = true, value = "author") String author,
+        @JsonProperty(required = true, value = "isbn13") String isbn13,
+        @JsonProperty(value = "author") String isbn10,
+        @JsonProperty(required = true, value = "language") String language,
+        @JsonProperty(required = true, value = "numberOfPages") Integer numberOfPages,
+        @JsonProperty(required = true, value = "description") String description,
+        @JsonProperty(required = true, value = "dateOfPublication") String dateOfPublication
+) {
+}
