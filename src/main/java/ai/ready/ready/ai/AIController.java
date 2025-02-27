ea4667518295ee198ae9ad483a3d5f6e062e594a
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2025-02-27 21:19:53 +0100

    feat: set up controller for ai requests

---
+++ src/main/java/ai/ready/ready/ai/AIController.java
@@ -0,0 +1,26 @@
+package ai.ready.ready.ai;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/ai")
+@AllArgsConstructor
+public class AIController {
+
+    private final AIService aiService;
+
+    @PostMapping("/find-book")
+    ResponseEntity<String> findBook(@RequestParam("image") MultipartFile image) {
+        if(image.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity.status(HttpStatus.FOUND).body(aiService.findBook(image));
+    }
+}
