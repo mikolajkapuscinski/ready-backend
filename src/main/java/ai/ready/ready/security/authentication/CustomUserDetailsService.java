0e0da6d81a16b3bdc44c5f29d165523a90c2c11b
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2025-03-02 21:18:49 +0100

    refactor: exception throwing

--- src/main/java/ai/ready/ready/security/authentication/CustomUserDetailsService.java
+++ src/main/java/ai/ready/ready/security/authentication/CustomUserDetailsService.java
@@ -17,10 +17,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email);
-        if (user == null) {
-            throw new UsernameNotFoundException(email);
-        }
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(email));
         return new UserDetailsModel(user);
     }
 }
