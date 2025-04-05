c827fae9d84934e7fe007204a88fadfe30e1877a
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2025-04-05 12:24:10 +0200

    feat: map roles to user details

--- src/main/java/ai/ready/ready/security/authentication/CustomUserDetailsService.java
+++ src/main/java/ai/ready/ready/security/authentication/CustomUserDetailsService.java
@@ -3,11 +3,17 @@ package ai.ready.ready.security.authentication;
 import ai.ready.ready.security.authentication.dto.UserDetailsModel;
 import ai.ready.ready.user.User;
 import ai.ready.ready.user.UserRepository;
+import ai.ready.ready.user.role.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -18,6 +24,12 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(email));
-        return new UserDetailsModel(user);
+        return new UserDetailsModel(user, getAuthorities(user.getRoles()));
+    }
+
+    private static List<GrantedAuthority> getAuthorities(List<Role> roles) {
+        return roles.stream().map(
+                role -> new SimpleGrantedAuthority(role.getName())
+        ).collect(Collectors.toList());
     }
 }
