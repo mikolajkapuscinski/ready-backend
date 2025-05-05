b0619beb2699894acafb9699d6b9f9b1fba33b43
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2025-05-05 16:56:03 +0200

    refactor: move JavaMailSender config to application.properties

--- src/main/java/ai/ready/ready/ReadyApplication.java
+++ src/main/java/ai/ready/ready/ReadyApplication.java
@@ -14,18 +14,4 @@ public class ReadyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReadyApplication.class, args);
 	}
-
-	@Bean
-	public JavaMailSender getJavaMailSender() {
-		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-		mailSender.setHost("smtp.gmail.com");
-		mailSender.setPort(587);
-		Properties props = mailSender.getJavaMailProperties();
-		props.put("mail.transport.protocol", "smtp");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.debug", "true");
-		return mailSender;
-	}
-
 }
