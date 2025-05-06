6f62ed7ba19a9d2b3cf1e51543251f939bc36993
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2025-05-06 22:59:25 +0200

    fix: delete unused imports

--- src/main/java/ai/ready/ready/ReadyApplication.java
+++ src/main/java/ai/ready/ready/ReadyApplication.java
@@ -2,11 +2,6 @@ package ai.ready.ready;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-
-import java.util.Properties;
 
 @SpringBootApplication
 public class ReadyApplication {
